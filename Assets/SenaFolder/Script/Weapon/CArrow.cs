519ebf46f29fc81b3827d9c4c9c296f35d8cbfb9
HALsenna <senna200091@gmail.com>
2022-10-13 19:09:46 +0900

    矢を複数個打てるようにする

--- Assets/SenaFolder/Script/Weapon/CArrow.cs
+++ Assets/SenaFolder/Script/Weapon/CArrow.cs
@@ -8,6 +8,7 @@ public class CArrow : MonoBehaviour
     private Rigidbody rb;           // 矢の剛体
     private float arrowForce = 0.0f;     // 矢を放つ力
     private GameObject objBow;
+    private int nArrowNum;          // 何番目の矢か
     #endregion
 
     #region serialize field
@@ -20,6 +21,7 @@ public class CArrow : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         // 弓のオブジェクトを取得
         objBow = GameObject.FindWithTag("Weapon");
+        nArrowNum = 0;
     }
 
     // Update is called once per frame
@@ -40,6 +42,27 @@ public class CArrow : MonoBehaviour
         Vector3 direction = -transform.up;
         rb.AddForce(direction * arrowForce, ForceMode.Impulse);        // 矢を発射する
         //Debug.Log("arrowForce" + arrowForce);
+    }
+    #endregion
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 地面に衝突したら矢を消滅させる
+        if(collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /*
+    * @brief 何番目の矢かどうかを設定する
+    * @param num 指定する番号
+    * @sa 矢を撃った時
+    */
+    #region set num
+    public void setNum(int index)
+    {
+        nArrowNum = index;
     }
     #endregion
 }
